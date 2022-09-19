ad382c6867b7f08b3c18200bab6c8ac79aa9140b
Sathish Selvam <sathishselvamdev@gmail.com>
2022-09-19 19:19:23 +0530

    19 Septemper

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
-import style from "../styles/custom.module.css";
+import "../styles/custom.module.css";
 
 class App extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
-      name: props.name,
+      firstName: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState(
+      (state, props) => {
+        return { [e.target.name]: e.target.value };
+      },
+      () => {
+        console.log("State Update is completed");
+      }
+    );
   }
 
   render() {
     return (
-      <div className={style.box}>
-        <h2>{this.props.name}</h2>
-        <input type="text" name="firstName" style={style.myInput} />
-      </div>
+      <>
+        <Header />
+        <fieldset>
+          <legend>{this.props.appTitle}</legend>
+          <label htmlFor="firstName" id="firstNameLabel">
+            First Name
+          </label>
+          <input type="text" id="firstName" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+        </fieldset>
+      </>
     );
   }
 }
 
-export { App };
+App.defaultProps = {
+  appTitle: "Personal Details",
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <h1>My Applications</h1>
+    </>
+  );
+};
+
+export default Header;
